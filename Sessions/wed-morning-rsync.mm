be162705efd6a0e4660bc8e1349e93da9f5c61af
tremblap <info@pierrealexandretremblay.com>
2022-03-17 17:54:46 +0100

    resync wed

---
+++ Sessions/wed-morning-rsync.mm
@@ -0,0 +1,95 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="sync" FOLDED="false" ID="ID_1441439406" CREATED="1647421388341" MODIFIED="1647429538228" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="what we said we&apos;d do you really want to do" POSITION="right" ID="ID_335469382" CREATED="1647421389188" MODIFIED="1647421412033">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="what next" POSITION="right" ID="ID_749816409" CREATED="1647421412435" MODIFIED="1647421416434">
+<edge COLOR="#0000ff"/>
+<node TEXT="own coding" ID="ID_1755412535" CREATED="1647421427290" MODIFIED="1647421430190"/>
+<node TEXT="reinforcing" ID="ID_1464403750" CREATED="1647421530800" MODIFIED="1647421536027">
+<node TEXT="audio query" ID="ID_1914993216" CREATED="1647421536369" MODIFIED="1647421539501">
+<node TEXT="std after" ID="ID_833049473" CREATED="1647421539939" MODIFIED="1647421543109"/>
+</node>
+</node>
+<node TEXT="deepning" ID="ID_705935318" CREATED="1647421544163" MODIFIED="1647421569277">
+<node TEXT="mlp audio desc and param" ID="ID_1447053194" CREATED="1647421569582" MODIFIED="1647421581506"/>
+</node>
+<node TEXT="ortho" ID="ID_1707686776" CREATED="1647421582226" MODIFIED="1647421585983">
+<node TEXT="decomp" ID="ID_233653710" CREATED="1647421586258" MODIFIED="1647421592002"/>
+</node>
+</node>
+<node TEXT="asb" POSITION="right" ID="ID_756934755" CREATED="1647421967384" MODIFIED="1647421971854">
+<edge COLOR="#ff00ff"/>
+<node TEXT="drums" ID="ID_10591899" CREATED="1647421972198" MODIFIED="1647421973503">
+<node TEXT="learning" ID="ID_442810731" CREATED="1647421985831" MODIFIED="1647421991475"/>
+</node>
+</node>
+<node TEXT="tension" POSITION="right" ID="ID_1807204978" CREATED="1647422041281" MODIFIED="1647422044384">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
