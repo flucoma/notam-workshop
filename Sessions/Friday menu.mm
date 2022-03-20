18bdf5eeedac8f54fc87ad9a0431326fc828274c
tremblap <info@pierrealexandretremblay.com>
2022-03-20 10:55:20 +0000

    update friday menu

---
+++ Sessions/Friday menu.mm
@@ -0,0 +1,108 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Friday menu" FOLDED="false" ID="ID_422972552" CREATED="1647595272798" MODIFIED="1647595384522" STYLE="oval">
+<font SIZE="10"/>
+<hook NAME="MapStyle" zoom="3.0">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="reinforcement/re-explanation" POSITION="right" ID="ID_1450764543" CREATED="1647595453669" MODIFIED="1647595466518">
+<edge COLOR="#0000ff"/>
+<node TEXT="Rod patch help" ID="ID_151501428" CREATED="1647596896375" MODIFIED="1647596902198"/>
+</node>
+<node TEXT="best practices" POSITION="right" ID="ID_303518476" CREATED="1647595630646" MODIFIED="1647595636360">
+<edge COLOR="#00ffff"/>
+<node TEXT="workflows" ID="ID_1357655867" CREATED="1647595636652" MODIFIED="1647595638605">
+<node TEXT="batch process" ID="ID_1094170301" CREATED="1647595641457" MODIFIED="1647595646054"/>
+</node>
+</node>
+<node TEXT="thinking about time" POSITION="right" ID="ID_169925469" CREATED="1647595507580" MODIFIED="1647595514397">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="shooting arrows" POSITION="right" ID="ID_618930102" CREATED="1647595294232" MODIFIED="1647595384520">
+<edge COLOR="#ff0000"/>
+<font SIZE="10"/>
+<node TEXT="different slicers" ID="ID_316680970" CREATED="1647595541195" MODIFIED="1647595548119">
+<node TEXT="curves" ID="ID_220632428" CREATED="1647595580599" MODIFIED="1647595582708"/>
+</node>
+<node TEXT="feature learning" ID="ID_335977362" CREATED="1647595344302" MODIFIED="1647595384523">
+<font SIZE="10"/>
+<node TEXT="kmeans" ID="ID_1395222084" CREATED="1647595354629" MODIFIED="1647595384523">
+<font SIZE="10"/>
+</node>
+<node TEXT="autoencoder" ID="ID_1109088689" CREATED="1647595299293" MODIFIED="1647595384523">
+<font SIZE="10"/>
+</node>
+<node TEXT="JITnmfclassifiyer" ID="ID_921372000" CREATED="1647595358689" MODIFIED="1647595384524">
+<font SIZE="10"/>
+</node>
+</node>
+<node TEXT="using PCA" ID="ID_965982860" CREATED="1647595475603" MODIFIED="1647595481936"/>
+<node TEXT="reaper integrationsssss" ID="ID_1003854293" CREATED="1647596000477" MODIFIED="1647596007923"/>
+</node>
+<node TEXT="review" POSITION="right" ID="ID_1767440746" CREATED="1647595566760" MODIFIED="1647595574217">
+<edge COLOR="#ff00ff"/>
+<node TEXT="week" ID="ID_565280885" CREATED="1647595574690" MODIFIED="1647595580259"/>
+<node TEXT="autobuf" ID="ID_1910775908" CREATED="1647595582986" MODIFIED="1647595586324"/>
+<node TEXT="learn" ID="ID_1527560928" CREATED="1647595647954" MODIFIED="1647595649132"/>
+</node>
+</node>
+</map>
